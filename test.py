6f824dcafd90e443f9a8a9a0958c197006b9d064
Malcolm Brooks <Malcolm.E.Brooks@metoffice.gov.uk>
2018-03-09 14:05:37 +0000

    Adds a link to the imt documentation (unless switched off)

--- test.py
+++ test.py
@@ -960,6 +960,7 @@ def __main__():
                                                           'Test ImageDict webpage',
                                                           preamble=webpage_preamble,
                                                           postamble=webpage_postamble,
+                                                          postamble_no_imt_link=True,
                                                           verbose=True, url_type='str')
     web_out[out_page_css] = imt.webpage.write_full_page(img_dict, out_page_css,
                                                         'Test ImageDict webpage with CSS',
