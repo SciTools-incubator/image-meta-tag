00ab00ed2d529d6098adf5c5ef00f5807ee67a69
Malcolm Brooks <Malcolm.E.Brooks@metoffice.gov.uk>
2018-07-05 14:07:35 +0100

    tidyup with a newline  between <div>s

--- ImageMetaTag/webpage.py
+++ ImageMetaTag/webpage.py
@@ -858,7 +858,7 @@ def write_js_placeholders(img_dict, file_obj=None, dict_depth=None, selector_pre
         else:
             img_div = '   <div id="the_image">Please wait while the page is loading.<br>{}</div>'
             file_obj.write(img_div.format(load_err_msg))
-        file_obj.write('   <div id="the_url">....</div>')
+        file_obj.write('\n   <div id="the_url">....</div>')
         # and finish off the placeholders:
         file_obj.write('''
    </font>
