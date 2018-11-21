d80c7c0833af569b573df4b7968ab0c97abef268
Malcolm Brooks <Malcolm.E.Brooks@metoffice.gov.uk>
2018-11-21 13:07:02 +0000

    Update version number to 0.7.8, update docs

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ for d, _, _ in os.walk(os.path.join(here, 'ImageMetaTag')):
 setup_args = dict(
     name = 'ImageMetaTag',
     # see release_process for details on incrementing the version
-    version = '0.7.7',
+    version = '0.7.8',
     description = 'Image metadata tagging, database and presentation',
     license = 'BSD3',
     author = 'Malcolm Brooks',
